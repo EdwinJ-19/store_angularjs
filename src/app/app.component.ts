f4cc990c586572258dfce5572bc8a02bf8825ec6
EdwinJ-19 <edwinjames2001@gmail.com>
2024-08-07 12:22:02 +0530

    productlist_done_added_footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductlistComponent } from './productlist/productlist.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProductlistComponent, NavbarComponent],
+  imports: [RouterOutlet, ProductlistComponent, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
