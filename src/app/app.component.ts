b764c7b7bd488f1883781fdadb74f97df535f05d
EdwinJ-19 <edwinjames2001@gmail.com>
2024-08-07 14:02:18 +0530

    footer_started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { ProductlistComponent } from './productlist/productlist.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProductlistComponent, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, ProductlistComponent, NavbarComponent, FooterComponent, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
