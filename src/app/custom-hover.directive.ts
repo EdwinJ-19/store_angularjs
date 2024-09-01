dcaf280a8dc7dff47ad88e792660ebf17b81bcb3
EdwinJ-19 <edwinjames2001@gmail.com>
2024-09-01 12:09:32 +0530

    fo

---
+++ src/app/custom-hover.directive.ts
@@ -0,0 +1,11 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appCustomHover]',
+  standalone: true
+})
+export class CustomHoverDirective {
+
+  constructor() { }
+
+}
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -3,11 +3,13 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+// import { CustomHoverBtnDirective } from '../custom-hover-btn.directive';
+import { CustomHoverDirective } from '../custom-hover.directive';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule,CustomHoverDirective],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
